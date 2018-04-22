9be25b0304fa4b0f97e50cced69bbc426dd0f186
Alexander Sidorenko <asidorenkoa@yandex.ru>
2018-04-22 14:02:24 +0300

    add tojpg script

---
+++ code_alex/tojpg.py
@@ -0,0 +1,26 @@
+data_dir = 'data/train'
+import glob
+import os
+paths = glob.glob(data_dir + '/*/*')
+gifs = glob.glob(data_dir + '/*/*.gif')
+print('All -> jpg')
+for path in paths:
+    resol = path.split('.')[-1]
+    if resol != 'jpg':
+        os.system("convert " + path + ' ' + path + '.jpg')
+        os.remove(path)
+        #print('Image %s removed' % path)
+        
+new_paths = glob.glob(data_dir + '/*/*')
+for path in new_paths:
+    if path.split('-')[0] in gifs:
+        if path[-6:] != '-0.jpg':
+            os.remove(path)
+            #print(path)
+            
+new_paths = glob.glob(data_dir + '/*/*')
+for path in new_paths:
+    if path[-6:] == '-0.jpg':
+        os.rename(path, path[:-6]+'.jpg')
+
+print('Done!')
