530d7078beeaa40e3450bd962f679271c02a864a
Yan Budakyan <astromid@gmail.com>
2018-04-22 14:33:38 +0300

    submit code

--- code_yan/submit.py
+++ code_yan/submit.py
@@ -10,11 +10,11 @@ from task_config import ROOT_DIR, TEST_DIR, TASK_NAME
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--name', help="Name of the network in format {base_clf}-{number}")
+    parser.add_argument('-n', '--name', help="Model file name in models_dir")
     parser.add_argument('-b', '--batch_size', type=int, default=16)
     args = parser.parse_args()
 
-    MODEL_DIR = os.path.join('models', args.name + '-' + TASK_NAME)
+    MODEL_DIR = os.path.join(ROOT_DIR, 'models_eval')
     SUB_DIR = os.path.join(ROOT_DIR, 'subs')
     TEST_CONFIG = {
         'batch_size': args.batch_size,
@@ -22,23 +22,20 @@ if __name__ == '__main__':
     }
     test_files = sorted(glob(os.path.join(TEST_DIR, '*', '*')))
     test_loader = ImageLoader(files=test_files, mode='test', **TEST_CONFIG)
-    model_files = sorted(glob(os.path.join(MODEL_DIR, '*.h5')))
-    print(f"Found {len(model_files)} models to evaluate")
-    for file in model_files:
-        K.clear_session()
-        model_name = os.path.basename(file)
-        model = load_model(file)
-        print(f"Model: {model_name}")
-        probs = model.predict_generator(
-            generator=test_loader,
-            steps=len(test_loader),
-            verbose=1)
-        ids = np.argmax(probs, axis=1)
-        labels = [test_loader.id2label[id_] for id_ in ids]
-        data = {
-            'file': test_loader.files,
-            'category': labels
-        }
-        sub_path = os.path.join(SUB_DIR, f'{file}-sub.csv')
-        pd.DataFrame(data).to_csv(sub_path, index=False)
-        print(f"Submit {sub_path} created successfully")
+    MODEL_PATH = os.path.join(MODEL_DIR, args.name)
+    model_name = os.path.basename(MODEL_PATH)
+    model = load_model(MODEL_PATH)
+    print(f"Model: {model_name}")
+    probs = model.predict_generator(
+        generator=test_loader,
+        steps=len(test_loader),
+        verbose=1)
+    ids = np.argmax(probs, axis=1)
+    labels = [test_loader.id2label[id_] for id_ in ids]
+    data = {
+        'file': test_loader.files,
+        'category': labels
+    }
+    sub_path = os.path.join(SUB_DIR, f'{model_name}-sub.csv')
+    pd.DataFrame(data).to_csv(sub_path, index=False)
+    print(f"Submit {sub_path} created successfully")
--- code_yan/task_config.py
+++ code_yan/task_config.py
@@ -4,6 +4,6 @@ import os
 ROOT_DIR = '..'
 TASK_NAME = 'smartmail'
 TRAIN_DIR = os.path.join(ROOT_DIR, 'data', 'train')
-TEST_DIR = None
+TEST_DIR = os.path.join(ROOT_DIR, 'data', 'test')
 CROP_SIZE = 299
 RESCALE_SIZE = 299
